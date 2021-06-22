237c453043ed3c665db1fd7d9480a534b5daa004
Ilya Davydenko <57727023+SometimesUseful@users.noreply.github.com>
2021-06-22 11:31:53 +0300

    Final

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 let list = document.querySelector('.tl-tasks');
 let addBtn = document.querySelector('.tl-input-btn');
 let input = document.querySelector('.tl-input');
-let iterator = 0;
 let li;
-window.onload = (function (){
+window.onload = function(){
     for (let i = 0; i < localStorage.length; i++) {
-        addElement(localStorage.getItem(localStorage.key(i.toString())));
+        if (localStorage.length!==0){
+            addElement(localStorage.getItem(localStorage.key(i)));
+        }
     }
-});
-
+}
 function addElement(value = 'default'){
-    for (let listItem of document.querySelectorAll('li')){
-        iterator++;
-        listItem.id = iterator.toString();
-    }
     if (value !== 'default'){
         li = document.createElement('li');
         li.classList.add('tl-task');
@@ -23,11 +19,7 @@ function addElement(value = 'default'){
         <button class="removeBtn">Remove</button>
         <button class="editBtn">Edit</button>
             `;
-        for (let listItem of document.querySelectorAll('li')){
-            iterator++;
-            listItem.id = iterator.toString();
-        }
-        localStorage.setItem(li.id, value);
+        li.id = "" + localStorage.length++;
     } else {
         li = document.createElement('li');
         li.classList.add('tl-task');
@@ -41,43 +33,13 @@ function addElement(value = 'default'){
             alert('You need to input a value');
             return;
         }
-        for (let listItem of document.querySelectorAll('li')){
-            iterator++;
-            listItem.id = iterator.toString();
-
-        }
+        li.id = "" + localStorage.length++;
         localStorage.setItem(li.id, input.value);
     }
     list.append(li);
-    for (const ch of document.querySelectorAll('.check')) {
-        ch.addEventListener('change',function (){
-            if (this.checked === true){
-                this.parentNode.style.textDecoration = 'line-through';
-            } else {
-                this.parentNode.style.textDecoration = 'none';
-            }
-        });
-    }
-    for (const removeBtn of document.querySelectorAll('.removeBtn')) {
-        removeBtn.addEventListener('click', function (){
-           this.parentNode.remove();
-           localStorage.removeItem(this.parentNode.id)
-        });
-    }
-    for (const editButton of document.querySelectorAll('.editBtn')) {
-        editButton.addEventListener('click', function (){
-            let newTask = prompt('Input a new task');
-            if (newTask === null){
-             return 0;
-            }
-            localStorage.setItem(this.parentNode.id, newTask);
-        });
-    }
-    for (let listItem of document.querySelectorAll('li')){
-        iterator++;
-        listItem.id = iterator.toString();
-    }
-    iterator = 0;
+    checkListener();
+    removeListener();
+    editListener();
     input.value = '';
 }
 addBtn.addEventListener('click',() => addElement());
@@ -87,3 +49,45 @@ input.addEventListener('keydown', (e) => {
         addElement();
     }
 })
+
+function editListener(){
+    for (const editButton of document.querySelectorAll('.editBtn')) {
+        editButton.addEventListener('click', function (){
+            let newTask = prompt('Input a new task');
+            if (newTask === null){
+                return 0;
+            }
+            li.innerHTML = `
+            <input type="checkbox" class="check">
+            ${newTask}
+        <button class="removeBtn">Remove</button>
+        <button class="editBtn">Edit</button>
+            `;
+            checkListener();
+            removeListener();
+            console.log(this.id);
+            localStorage.setItem(li.id, newTask);
+        });
+    }
+}
+
+function removeListener(){
+    for (const removeBtn of document.querySelectorAll('.removeBtn')) {
+        removeBtn.addEventListener('click', function (){
+            this.parentNode.remove();
+            localStorage.removeItem(this.parentNode.id);
+        });
+    }
+}
+
+function checkListener(){
+    for (const ch of document.querySelectorAll('.check')) {
+        ch.addEventListener('change',function (){
+            if (this.checked === true){
+                this.parentNode.style.textDecoration = 'line-through';
+            } else {
+                this.parentNode.style.textDecoration = 'none';
+            }
+        });
+    }
+}
