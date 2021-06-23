a5d1ef859b919a70af96b6004317096aace1b0c7
Ilya Davydenko <57727023+SometimesUseful@users.noreply.github.com>
2021-06-23 12:03:02 +0300

    No localStorage

--- script.js
+++ script.js
@@ -1,93 +1,60 @@
 let list = document.querySelector('.tl-tasks');
 let addBtn = document.querySelector('.tl-input-btn');
 let input = document.querySelector('.tl-input');
-let li;
-window.onload = function(){
-    for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.length!==0){
-            addElement(localStorage.getItem(localStorage.key(i)));
-        }
+
+function addElement(){
+    if (input.value === ''){
+        alert('Enter a task');
+        return;
     }
-}
-function addElement(value = 'default'){
-    if (value !== 'default'){
-        li = document.createElement('li');
-        li.classList.add('tl-task');
-        li.innerHTML = `
-            <input type="checkbox" class="check">
-            ${value}
-        <button class="removeBtn">Remove</button>
-        <button class="editBtn">Edit</button>
-            `;
-        li.id = "" + localStorage.length++;
-    } else {
-        li = document.createElement('li');
-        li.classList.add('tl-task');
-        li.innerHTML = `
-            <input type="checkbox" class="check">
-            ${input.value}
-        <button class="removeBtn">Remove</button>
-        <button class="editBtn">Edit</button>
-            `;
-        if (input.value === ''){
-            alert('You need to input a value');
-            return;
-        }
-        li.id = "" + localStorage.length++;
-        localStorage.setItem(li.id, input.value);
-    }
+    let li = document.createElement('li');
+    li.classList.add('tl-task');
+    let removeBtn = document.createElement('button');
+    removeBtn.innerText = 'Remove';
+    removeBtn.classList.add('removeBtn');
+    let editBtn = document.createElement('button');
+    editBtn.classList.add('editBtn');
+    editBtn.innerText = 'Edit';
+    let check = document.createElement('input');
+    check.type = 'checkbox';
+    check.classList.add('check');
+    let inputValue = document.createElement('span');
+    inputValue.classList.add('task-value');
+    inputValue.innerText = input.value;
+    li.append(check);
+    li.append(inputValue);
+    li.append(removeBtn);
+    li.append(editBtn);
     list.append(li);
-    checkListener();
-    removeListener();
-    editListener();
+    check.addEventListener('change',checkListener);
+    removeBtn.addEventListener('click', function (){
+        this.parentNode.remove();
+    });
+    editBtn.addEventListener('click',editBtnListener)
     input.value = '';
 }
-addBtn.addEventListener('click',() => addElement());
+
+function editBtnListener(){
+    let taskValue = document.querySelector('.task-value');
+    let newValue = prompt('Enter new task', this.parentNode.childNodes[1].innerText);
+    if (newValue.length === 0){
+        return;
+    }
+    this.parentNode.childNodes[1].innerText = newValue;
+}
+
+function checkListener(){
+    if (this.checked === true){
+        this.parentNode.style.textDecoration = 'line-through';
+    } else {
+        this.parentNode.style.textDecoration = 'none';
+    }
+}
+
+addBtn.addEventListener('click', () => addElement());
 input.addEventListener('keydown', (e) => {
     if (e.keyCode === 13){
         e.preventDefault();
         addElement();
     }
-})
-
-function editListener(){
-    for (const editButton of document.querySelectorAll('.editBtn')) {
-        editButton.addEventListener('click', function (){
-            let newTask = prompt('Input a new task');
-            if (newTask === null){
-                return 0;
-            }
-            li.innerHTML = `
-            <input type="checkbox" class="check">
-            ${newTask}
-        <button class="removeBtn">Remove</button>
-        <button class="editBtn">Edit</button>
-            `;
-            checkListener();
-            removeListener();
-            console.log(this.id);
-            localStorage.setItem(li.id, newTask);
-        });
-    }
-}
-
-function removeListener(){
-    for (const removeBtn of document.querySelectorAll('.removeBtn')) {
-        removeBtn.addEventListener('click', function (){
-            this.parentNode.remove();
-            localStorage.removeItem(this.parentNode.id);
-        });
-    }
-}
-
-function checkListener(){
-    for (const ch of document.querySelectorAll('.check')) {
-        ch.addEventListener('change',function (){
-            if (this.checked === true){
-                this.parentNode.style.textDecoration = 'line-through';
-            } else {
-                this.parentNode.style.textDecoration = 'none';
-            }
-        });
-    }
-}
+});
